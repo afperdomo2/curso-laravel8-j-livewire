74da87f4b48c7d1f5c786544b81d7a44a5cf6647
Andrés Perdomo <alvarado0925@gmail.com>
2022-05-31 22:37:56 -0500

    🧪Feat: Create header and footer for web layout

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -8,10 +8,33 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+    <header class="shadow-lg">
+        <div class="bg-cyan-900 pb-1">
+            <nav class="bg-cyan-600 py-2">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/isotipo-platzi.png') }}" class="h-6 mx-auto">
+                </a>
+            </nav>
+        </div>
+    </header>
     <main class="py-10">
         <div class="container mx-auto px-4">
             @yield('content')
         </div>
     </main>
+    <footer class="py-4 text-center">
+        @auth
+            <a href="{{ url('dashboard') }}" class="text-sm text-gray-700 underline">
+                Dashboard
+            </a>
+        @else
+            <a href="{{ url('login') }}" class="text-sm text-gray-700 underline">
+                Login
+            </a>
+            <a href="{{ url('register') }}" class="ml-4 text-sm text-gray-700 underline">
+                Registro
+            </a>
+        @endauth
+    </footer>
 </body>
 </html>
