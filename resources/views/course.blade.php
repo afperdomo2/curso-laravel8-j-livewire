f68e7ea633b7d7de2b3a7d0a7085c4402f01d5a7
Andrés Perdomo <alvarado0925@gmail.com>
2022-06-01 23:28:50 -0500

    🧪Feat: List posts in the course description

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,7 +7,16 @@
                 <li class="font-medium text-sm text-gray-400 uppercase mb-4">
                     Contenido
                 </li>
-                ----
+                @foreach ($course->posts as $post)
+                <li class="flex items-center text-gray-600 mt-2">
+                    {{ $loop->index + 1 }}.&ensp;{{ $post->name }}
+                    @if ($post->free)
+                        <span class="text-xs text-white font-semibold bg-green-500 rounded ml-auto px-1">
+                            Gratis
+                        </span>
+                    @endif
+                </li>
+                @endforeach
             </ul>
         </div>
         <div class="text-gray-700 col-span-2">
--- resources/views/livewire/course-list.blade.php
+++ resources/views/livewire/course-list.blade.php
@@ -2,6 +2,7 @@
     @foreach ($courses as $course)
         <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
             <a href="{{ route('course', $course->slug) }}">
+
                 <img src="{{ $course->image }}" class="rounded-md mb-2">
                 <h2 class="text-left text-lg text-gray-600 truncate uppercase mb-2">
                     {{ $course->name }}
@@ -10,10 +11,18 @@
                     {{ $course->excerpt }}
                 </h3>
 
-                <div>
-                    <img src="{{ $course->user->avatar }}" class="rounded-full mt-4 mx-auto h-8 w-8">
-                    <p>{{ $course->user->name }}</p>
+                <div class="flex mt-4">
+                    <img src="{{ $course->user->avatar }}" class="h-10 w-10 rounded-full mr-2">
+                    <div class="text-left">
+                        <p class="text-gray-500 text-sm">
+                            {{ $course->user->name }}
+                        </p>
+                        <p class="text-gray-400 text-xs">
+                            {{ $course->created_at->diffForHumans() }}
+                        </p>
+                    </div>
                 </div>
+
             </a>
         </div>
     @endforeach
